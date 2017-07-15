b9b471b5af80ca9fe68e335c6cae0f5d9688fa94
OSavenkov <notorious.kg@gmail.com>
2017-07-16 01:38:54 +0600

    sequelize-mock

--- src/config.js
+++ src/config.js
@@ -1,23 +1,25 @@
 const Sequelize = require('sequelize');
+const SequelizeMock = require('sequelize-mock');
 
-const sequelize = new Sequelize('KaspiBusiness_Dev', 'KaspiBusinessUser', '123', {
-    host: 'kon-dvl-db',
-    dialect: 'mssql',
-    pool: {
-        max: 5,
-        min: 0,
-        idle: 10000
-    },
-});
+const sequelize = new SequelizeMock();
+// new Sequelize('KaspiBusiness_Dev', 'KaspiBusinessUser', '123', {
+//     host: 'kon-dvl-db',
+//     dialect: 'mssql',
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         idle: 10000
+//     },
+// });
 
-sequelize
-    .authenticate()
-    .then(() => {
-        console.log('Connection has been established successfully.');
-    })
-    .catch(err => {
-        console.error('Unable to connect to the database:', err);
-    });
+// sequelize
+//     .authenticate()
+//     .then(() => {
+//         console.log('Connection has been established successfully.');
+//     })
+//     .catch(err => {
+//         console.error('Unable to connect to the database:', err);
+//     });
 
 var config = {
     expressPort: 3000,
--- src/model/Organization.js
+++ src/model/Organization.js
@@ -1,12 +1,18 @@
-const Sequelize = require('sequelize');
 var config = require('../config.js');
 
-const Organization = config.sequelize.define('organization', {
-    Name: {
-        type: Sequelize.STRING
-    }
-}, {
-    timestamps: false
-});
+const Organization = config.sequelize
+.define('organization', {
+    Id : 1,
+    Name : 'Google'
+},
+{timestamps: false});
+
+//config
+//     .sequelize
+//     .define('organization', {
+//         Name: {
+//             type: Sequelize.STRING
+//         }
+//     }, {timestamps: false});
 
 module.exports = Organization;
