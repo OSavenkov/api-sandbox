f8ab0a7d4673b4454e99786b4bc3f9161335a3fe
oleg savenkov <notorious.kg@gmail.com>
2017-07-17 22:04:12 +0600

    aa

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ var config = require('./config.js');
 var SearchController = require('./routes/SearchController');
 
 var app = express();
-// "node-sspi": "^0.1.15",
+
 // CORS
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", config.allowesOrigins);
